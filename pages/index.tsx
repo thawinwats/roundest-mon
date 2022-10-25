92ce3c0cb786b603f6ffd266b3ecac867f3ccf53
Thawinwats Uggaravisee <thawinwats.ugg@satangcorp.com>
2022-10-26 00:11:33 +0700

    🎨 Adding basic UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-center mb-2">Witch Pokémon Rounder?</div>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-14 h-16 bg-slate-200"></div>
+        <div className="p-8">VS</div>
+        <div className="w-14 h-16 bg-slate-200"></div>
+      </div>
+    </div>
+  )
 }
