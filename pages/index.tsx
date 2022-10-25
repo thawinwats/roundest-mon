d37c5da4e68ec9181a5736cfa2cd391778be32e3
Thawinwats Uggaravisee <thawinwats.ugg@satangcorp.com>
2022-10-25 23:56:42 +0700

    🎨 Add default page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline bg-red-400">Hello world!</h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
